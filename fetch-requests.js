106ee86830f02d1ae85eb6eeb24a9edda4ccef15
Royce Calvin <royce20002006@yahoo.com>
2024-03-13 23:05:15 -0500

    finished

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,38 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here 
+fetch('/products', {
+  method: 'POST',
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+});
 
 
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here 
+fetch('/products', {
+  method: 'POST',
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'} })
+      // status code is 200
+      .then((res) =>  res.status)
+      // content type is text/html
+      .then(res => res.headers.get('content-type'))
+      //"http://localhost:5000/products/15"
+      .then(res => res.url)
+      //true
+      .then(res => res.redirected)
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here 
+fetch('/products', {
+  method: 'POST',
+  body: new URLSearchParams( { name: "Caribbean Delight Coffee", description: 'Made by Manatee Coffee', price: 11.99, categories: 'grocery'}),
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'} }).then(res => res.url)
